51170974e1b23e14a6a725a4b8ef5a0aa4476a60
OllieHall01 <124044423+OllieHall01@users.noreply.github.com>
2023-02-28 10:50:00 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -12,21 +12,7 @@ function HardRight () {
     maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 60)
     basic.pause(100)
 }
-function Straight () {
-    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 60)
-}
-function SoftRight () {
-    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 60)
-    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 30)
-}
-function Stop () {
-    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 0)
-}
-function SoftLeft () {
-    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 30)
-    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 60)
-}
-basic.forever(function () {
+function LineFollowing () {
     LFSR = maqueen.readPatrol(maqueen.Patrol.PatrolRight)
     LFSL = maqueen.readPatrol(maqueen.Patrol.PatrolLeft)
     if (LFSL == 1 && LFSR == 1) {
@@ -37,5 +23,43 @@ basic.forever(function () {
         SoftRight()
     } else {
         Straight()
+    }
+}
+function AvoidingObject () {
+    while (maqueen.Ultrasonic(PingUnit.Centimeters) < 5) {
+        SoftRight()
+    }
+    while (maqueen.Ultrasonic(PingUnit.Centimeters) > 10) {
+        SoftLeft()
+    }
+}
+function ObjectAvoidance () {
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 50)
+    basic.pause(200)
+    while (maqueen.Ultrasonic(PingUnit.Centimeters) < 10) {
+        Stop()
+    }
+}
+function Straight () {
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 60)
+}
+function SoftRight () {
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 60)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 30)
+}
+function Stop () {
+    maqueen.motorStop(maqueen.Motors.All)
+}
+function SoftLeft () {
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 30)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 60)
+}
+basic.forever(function () {
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) < 10) {
+        ObjectAvoidance()
+        AvoidingObject()
+    }
+    if ((LFSL || LFSR) == 1) {
+        LineFollowing()
     }
 })
